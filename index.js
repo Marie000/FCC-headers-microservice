32c3d52995c8320ca2a206b10ffd325262ab3774
Marie Pelletier <marie.pelletier@gmail.com>
2016-07-22 14:37:07 -0400

    all working?

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ var PORT = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
   var ip = req.headers["x-forwarded-for"];
-  res.json(ip);
+  var agent = useragent.parse(req.headers['user-agent']);
+  var os = agent.os.toString();
+  var languages = req.headers["accept-language"];
+  var lg = languages.split(',')[0];
+  var output = {
+    "ip": ip,
+    "operating system": os,
+    "language": lg
+  };
+  res.json(output);
 })
 
 app.listen(PORT);
